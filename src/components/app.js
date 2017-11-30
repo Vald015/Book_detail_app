e21ea79a66ebc832ab26e87e9e4169ad12e2fa61
Osvaldo <osvaldo.ramirez@upr.edu>
2017-11-29 21:02:31 -0400

    Completed book detail app using react and redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import BookList from '../containers/book-list'; // .. means we go up on the directory
+import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <BookDetail />
       </div>
     );
   }
