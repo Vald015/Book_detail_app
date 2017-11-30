01fed9738afd9e3c6b9c9998c5c852d6dcd8103e
Osvaldo <osvaldo.ramirez@upr.edu>
2017-11-29 20:17:52 -0400

    Active book reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,11 @@
 export function selectBook(book){
-  console.log('A book has been selected:', book.title);
+  //console.log('A book has been selected:', book.title);
+  //selectBookis an ActionCreator, it needs to return an action.
+  //An object with an type property.
+  //type always on caps
+  //payload more info about the action taken
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
